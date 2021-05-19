fix scaling

pyxconv/probe.py
@@ -4,11 +4,11 @@ from typing import List
 
 
 @torch.jit.script
-def rand_with_zeros(N: int, ps:int, indices, X):
+def rand_with_zeros(N: int, ps:int, scale: float, indices, X):
     n = indices.shape[0]
     a = torch.zeros(N, ps, device=X.device)
     a[:, indices] = torch.randn(N, n, device=X.device)
-    return torch.sqrt(ps/n)*a
+    return torch.sqrt(ps/(scale*n))*a
 
 
 @torch.jit.script
@@ -16,10 +16,12 @@ def draw_e(ps:int, ci: int, N: int, X):
     if ps // ci > 8:
         n = ps // ci
         inds = torch.split(torch.randperm(ps, dtype=torch.long), n)
+        scale = 1.
     else:
         n = 8
         inds = torch.split(torch.randperm(n*ci, dtype=torch.long) % ps, n)
-    e = torch.cat([rand_with_zeros(N, ps, inds[i], X) for i in range(ci)], dim=0)
+        scale = n / (ps // ci)
+    e = torch.cat([rand_with_zeros(N, ps, scale, inds[i], X) for i in range(ci)], dim=0)
     return e
 
 @torch.jit.script
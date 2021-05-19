b964be596c8c104e2d1cc3bddf1ea16d18c041a5
mloubout <mathias.louboutin@gmail.com>
2021-05-19 12:26:39 -0400

    fix scaling

--- experiments/multi_chan_probe.jl
+++ experiments/multi_chan_probe.jl
@@ -1,61 +1,68 @@
-using LinearAlgebra, PyPlot, Random, FFTW, Distributions, CircularArrays, Printf
+using LinearAlgebra, PyPlot, Random, FFTW, Distributions, CircularArrays, Printf, Statistics
 
 close(:all)
 
-ci, co, N, ps, b = 16, 16, 32*32, 64, 10
+ci, co, N, ps, b = 32, 32, 1024, 128, 50
 
-eb = sign.(randn(Float32, co*N, ps))
+eb = randn(Float32, co*N, ps)
 
 chunk(arr, n) = [arr[i:min(i + n - 1, end)] for i in 1:n:length(arr)]
 simil(x, y) = 100*dot(x[:], y[:])/(norm(x)*norm(y))
-error(x, y) = norm(x - y)
+error(x, y) = norm(x - y)/norm(x)
 
 function draw_e(ps::Integer, co::Integer, N::Integer)
     n = ps ÷ co
     if n < 8
         n = 8
         inds = chunk(randperm(n*co) .% ps .+ 1, n)
+        overlap = n / (ps ÷ co)
     else
         inds = chunk(randperm(ps), n)
+        overlap = 1
     end
     e = zeros(Float32, N*co, ps)
     for i=1:co
-        # iloc = inds[(i-1)*n+1:i*n]
         iloc = inds[i]
         ni = length(iloc)
-        e[(i-1)*N+1:i*N, iloc] .= Float32(sqrt(ps/(ni)))*sign.(randn(Float32, N, ni))
+        e[(i-1)*N+1:i*N, iloc] .= Float32(sqrt(ps/(overlap*ni)))*randn(Float32, N, ni)
     end
-    e = e
-    return e, collect(e')
+
+    return e
 end
 
-eortho, eorthot = draw_e(ps, co, N)
+eortho = draw_e(ps, co, N)
 
-figure()
-subplot(221)
-imshow(eb, vmin=-ps/10, vmax=ps/10, cmap="seismic", aspect="auto")
-title("Z, Random")
-subplot(222)
-imshow(eortho, vmin=-ps/10, vmax=ps/10, cmap="seismic", aspect="auto")
-title("Z, Random Orthogonalized")
-subplot(223)
-imshow(eb*eb', vmin=-ps/10, vmax=ps/10, cmap="seismic", aspect="auto")
-title(L"$ZZ^T$ Random")
-subplot(224)
-imshow(eortho*eorthot, vmin=-ps/10, vmax=ps/10, cmap="seismic", aspect="auto")
-title(L"$ZZ^T$ Random Orthogonalized")
-tight_layout()
+# figure()
+# subplot(221)
+# imshow(eb, vmin=-ps/10, vmax=ps/10, cmap="seismic", aspect="auto")
+# title("Z, Random")
+# subplot(222)
+# imshow(eortho, vmin=-ps/10, vmax=ps/10, cmap="seismic", aspect="auto")
+# title("Z, Random Orthogonalized")
+# subplot(223)
+# imshow(eb*eb', vmin=-ps/10, vmax=ps/10, cmap="seismic", aspect="auto")
+# title(L"$ZZ^T$ Random")
+# subplot(224)
+# imshow(eortho*eorthot, vmin=-ps/10, vmax=ps/10, cmap="seismic", aspect="auto")
+# title(L"$ZZ^T$ Random Orthogonalized")
+# tight_layout()
 
 ####### PROBE 3D Tensore with it
-probe_trace(ci::Integer, co::Integer, N::Integer, A::Matrix{T}, e::Matrix{T}, et) where T = probe_trace(ci, co, N, A, e, collect(et))
 
-function probe_trace(ci::Integer, co::Integer, N::Integer, A::Matrix{T}, e::Matrix{T}, et::Matrix{T}=e') where T
+function rescale(A::outer_LR, tr_est::Matrix{T}) where T
+    # In practice can be done in the same way as probing, get mean of X in forward then backward
+    est_scale =sum(mean(A.L[1:N, :], dims=1).*A.R[1:N, :])
+    act_scale = tr_est[1, 1]
+    return est_scale/act_scale .* tr_est
+end
+
+function probe_trace(ci::Integer, co::Integer, N::Integer, A::outer_LR, e::Matrix{T}) where T
     # A * z
     xyz = A * e
     trl = zeros(T, ci, co)
     for cii = 1:ci
         for coi = 1:co
-            eloc = view(et, :, (coi - 1)*N+1:coi*N)
+            eloc = view(e, (coi - 1)*N+1:coi*N, :)'
             xyxloc = view(xyz, (cii - 1)*N+1:cii*N, :)
             trl[cii, coi] = LinearAlgebra.tr(eloc * xyxloc)/ps
         end
@@ -63,15 +70,30 @@ function probe_trace(ci::Integer, co::Integer, N::Integer, A::Matrix{T}, e::Matr
     return trl
 end
 
-A = vcat([hcat([rand(-100:100) * randn(Float32, N, N) for i=1:co]...) for j=1:ci]...)
-block(A, i, j) = A[(i-1)*N+1:i*N, (j-1)*N+1:j*N]
+
+struct outer_LR
+    L
+    R
+end
+
+Base.:*(A::outer_LR, x) = A.L*(A.R'*x)
+Base.:*(x, A::outer_LR) = (x*A.L)*A.R'
+block(A::outer_LR, i, j) = view(A.L, (i-1)*N+1:i*N, :) * view(A.R, (j-1)*N+1:j*N, :)'
+
+a = vcat([rand(-5:5) * randn(Float32, N, b) for i=1:ci]...)
+
+A = outer_LR(a, max.(0f0, 100 .* randn(Float32, N*co, b)))
+# figure()
+# imshow(A, cmap="seismic", vmin=-1, vmax=1)
+# colorbar()
+# title(L"XY^T")
 
 true_tr = zeros(Float32, ci, co)
 [true_tr[i, j] = LinearAlgebra.tr(block(A, i, j)) for i=1:ci for j=1:co]
 
 # Single sample
 trr = probe_trace(ci, co, N, A, eb)
-tro = probe_trace(ci, co, N, A, eortho, eorthot)
+tro = probe_trace(ci, co, N, A, eortho)
 
 function plot_tr(trr, tro, true_tr)
     iplot = rand(1:ci, 4)
@@ -87,11 +109,10 @@ function plot_tr(trr, tro, true_tr)
 end
 
 plot_tr(trr, tro, true_tr)
-# me = eb*eb'
-# meo = eortho*eorthot
 
 # Mean
-np = 200
+np = 50
+
 convergence = zeros(Float32, np, 2)
 similarity = zeros(Float32, np, 2)
 convergence[1, 1] = error(true_tr, trr)
@@ -99,14 +120,18 @@ convergence[1, 2] = error(true_tr, tro)
 
 similarity[1, 1] = simil(true_tr, trr)
 similarity[1, 2] = simil(true_tr, tro)
+
+i=1
+
+@printf("%d/%d : Er = %2.2e, Sr = %2.2f, Eo = %2.2e, So = %2.2f \n",i, np,
+        convergence[i, 1], similarity[i, 1], convergence[i, 2], similarity[i, 2])
+
 for i=2:np
     local eeb = randn(Float32, co*N, ps)
-    local eeortho, eeorthot = draw_e(ps, co, N)
-    # global me = ((i-1)*me + eeb*eeb')/i
-    # global meo = ((i-1)*meo + eeortho*eeorthot)/i
+    local eeortho = draw_e(ps, co, N)
 
     global trr = ((i-1)*trr + probe_trace(ci, co, N, A, eeb))/i
-    global tro = ((i-1)*tro + probe_trace(ci, co, N, A, eeortho, eeorthot))/i
+    global tro = ((i-1)*tro + probe_trace(ci, co, N, A, eeortho))/i
     convergence[i, 1] = error(true_tr, trr)
     convergence[i, 2] = error(true_tr, tro)
     similarity[i, 1] = simil(true_tr, trr)
@@ -116,14 +141,6 @@ for i=2:np
 end
 
 ni(x) = x / norm(x, Inf)
-
-# figure()
-# subplot(121)
-# imshow(me, vmin=-ps/10, vmax=ps/10, cmap="seismic")
-# title(L"Mean Random $ZZ^T$")
-# subplot(122)
-# imshow(meo, vmin=-ps/10, vmax=ps/10, cmap="seismic")
-# title(L"Mean Random Orthogonalized $ZZ^T$")
 
 plot_tr(trr, tro, true_tr)
 
@@ -138,8 +155,9 @@ loglog(convergence[1:np, 1], label="Error random")
 loglog(convergence[1:np, 2], label="Error random ortho")
 legend()
 subplot(212)
-semilogx(similarity[1:np, 1], label="Similarity random")
-semilogx(similarity[1:np, 2], label="Similarity random ortho")
+loglog(similarity[1:np, 1], label="Similarity random")
+loglog(similarity[1:np, 2], label="Similarity random ortho")
+ylim([25.0, 100.0])
 legend()
 
 
--- pyxconv/probe.py
+++ pyxconv/probe.py
@@ -4,11 +4,11 @@ from typing import List
 
 
 @torch.jit.script
-def rand_with_zeros(N: int, ps:int, indices, X):
+def rand_with_zeros(N: int, ps:int, scale: float, indices, X):
     n = indices.shape[0]
     a = torch.zeros(N, ps, device=X.device)
     a[:, indices] = torch.randn(N, n, device=X.device)
-    return torch.sqrt(ps/n)*a
+    return torch.sqrt(ps/(scale*n))*a
 
 
 @torch.jit.script
@@ -16,10 +16,12 @@ def draw_e(ps:int, ci: int, N: int, X):
     if ps // ci > 8:
         n = ps // ci
         inds = torch.split(torch.randperm(ps, dtype=torch.long), n)
+        scale = 1.
     else:
         n = 8
         inds = torch.split(torch.randperm(n*ci, dtype=torch.long) % ps, n)
-    e = torch.cat([rand_with_zeros(N, ps, inds[i], X) for i in range(ci)], dim=0)
+        scale = n / (ps // ci)
+    e = torch.cat([rand_with_zeros(N, ps, scale, inds[i], X) for i in range(ci)], dim=0)
     return e
 
 @torch.jit.script
